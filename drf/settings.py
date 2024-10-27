62932da691ae08d4ccd7b65fd0088396f8c57bf6
alovidal <alo.vidal@duocuc.cl>
2024-10-27 17:50:55 -0300

    funcional

--- drf/settings.py
+++ drf/settings.py
@@ -125,6 +125,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {  
-    'DEFAULT_SCHEMA_CLASS' = 'rest_framework.schemas.coreapi.AutoSchema'
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
